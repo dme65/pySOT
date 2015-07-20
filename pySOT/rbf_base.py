Fixed a bug in the capped surface. Added scaling of the distances so that no points are ever further away than distance 1 from a center Added regularization to the RBF interplant Added more kernels but they aren't activated yet

pySOT/rbf_base.py
@@ -144,7 +144,8 @@ class BaseRBFSystem(object):
     def refactor(self):
         """Compute factorization
         """
-        self.lupiv = la.lu_factor(self.M)
+        eta = 1e-16 * np.eye(self.M.shape[0]) * la.norm(self.M, 1) * la.norm(self.M, np.inf)
+        self.lupiv = la.lu_factor(self.M + eta)
 
     def solve(self, rhs):
         """Solve with the system matrix
@@ -217,7 +218,7 @@ class SimpleRBFSystem(BaseRBFSystem):
         elif dpoly == 1:
             mtail = 1
         elif dpoly == 2:
-            mtail = dim+1
+            mtail = dim + 1
         super(SimpleRBFSystem, self).__init__(dim, mtail, maxpts)
 
     def fill_basis(self, B, y, d=None):

pySOT/rbf_interpolant.py
@@ -10,9 +10,7 @@
 import numpy as np
 import numpy.linalg as la
 import scipy.spatial as scp
-
 from rbf_surfaces import CubicRBFSurface
-
 
 class RBFInterpolant(object):
     """Compute and evaluate RBF interpolant.
@@ -56,7 +54,7 @@ class RBFInterpolant(object):
     def __init__(self, surftype=CubicRBFSurface, eta=1e-8, maxp=100):
         self.maxp = maxp
         self.surface = None
-        self.surftype = CubicRBFSurface
+        self.surftype = surftype
 
     @property
     def nump(self):
@@ -105,8 +103,8 @@ class RBFInterpolant(object):
 
         :param fx: Transformed function value
         """
-        assert self.surface.fx.shape == fx.shape
-        self.surface.set_fx(fx)
+        assert self.surface.npt == fx.shape[0]
+        self.surface.set_fx(fx.ravel())
 
     def eval(self, xx):
         """Evaluate the rbf interpolant at the point xx
@@ -154,7 +152,7 @@ def _main():
                         np.sin(x[0])-x[0]*np.sin(x[1])])
         return dfx
 
-    fhat = RBFInterpolant(1e-8, 20)
+    fhat = RBFInterpolant(CubicRBFSurface, 1e-8, 20)
     xs = np.random.rand(120, 2)
     for i in range(100):
         xx = xs[i, :]

pySOT/rbf_surfaces.py
@@ -18,11 +18,12 @@ class LinearRBFSystem(SimpleRBFSystem):
     """Linear RBF system"""
 
     dpoly = 1
+    avgdist = False
 
-    def phi(self, r):
+    def phi(self, r, epsilon=None):
         return r
 
-    def dphi_div_r(self, r):
+    def dphi_div_r(self, r, epsilon=None):
         return (r*0 + 1)/r
 
 
@@ -30,26 +31,79 @@ class CubicRBFSystem(SimpleRBFSystem):
     """Cubic RBF system"""
 
     dpoly = 2
+    avgdist = False
 
-    def phi(self, r):
+    def phi(self, r, epsilon=None):
         return r ** 3
 
-    def dphi_div_r(self, r):
+    def dphi_div_r(self, r, epsilon=None):
         return 3 * r
+
+class QuinticRBFSystem(SimpleRBFSystem):
+    """Quintic RBF system"""
+
+    dpoly = 2
+    avgdist = False
+
+    def phi(self, r, epsilon=None):
+        return r ** 5
+
+    def dphi_div_r(self, r, epsilon=None):
+        return 5 * (r ** 3)
 
 
 class ThinPlateRBFSystem(SimpleRBFSystem):
     """Thin plate RBF system"""
 
     dpoly = 2
-    eps = np.finfo(np.double).tiny
+    avgdist = False
+    tiny = np.finfo(np.double).tiny
 
-    def phi(self, r):
-        return r * r * np.log(r+self.eps)
+    def phi(self, r, epsilon=None):
+        return r * r * np.log(r + self.tiny)
 
-    def dphi_div_r(self, r):
-        return 2 * np.log(r+self.eps) + 1
+    def dphi_div_r(self, r, epsilon=None):
+        return 2 * np.log(r + self.tiny) + 1
 
+"""
+class MultiQuadraticRBFSystem(SimpleRBFSystem):
+    " Multi-Quadratic RBF "
+
+    dpoly = 2
+    avgdist = True
+
+    def phi(self, r, epsilon):
+        return np.sqrt((1.0/epsilon * r) ** 2 + 1)
+
+    def dphi_div_r(self, r, epsilon):
+        return 1.0/((epsilon ** 2) * np.sqrt(1 + (1.0/epsilon * r) ** 2))
+
+
+class InverseMultiQuadraticRBFSystem(SimpleRBFSystem):
+    " Inverse Multi-Quadratic RBF "
+
+    dpoly = 2
+    avgdist = True
+
+    def phi(self, r, epsilon):
+        return 1.0/np.sqrt((1.0 / epsilon * r) ** 2 + 1)
+
+    def dphi_div_r(self, r, epsilon):
+        return - 1.0/((epsilon ** 2) * (np.sqrt(1 + (1.0/epsilon * r) ** 2)) ** 3)
+
+
+class GaussianRBFSystem(SimpleRBFSystem):
+    " Gaussian RBF "
+
+    dpoly = 2
+    avgdist = True
+
+    def phi(self, r, epsilon):
+        return np.exp(-(1.0 / epsilon * r) ** 2)
+
+    def dphi_div_r(self, r, epsilon):
+        return - (2.0/(epsilon**2)) * np.exp(-(1.0 / epsilon * r) ** 2)
+"""
 
 class LinearRBFSurface(SimpleRBFSurface):
     RBFSystem = LinearRBFSystem
@@ -59,9 +113,26 @@ class CubicRBFSurface(SimpleRBFSurface):
     RBFSystem = CubicRBFSystem
 
 
+class QuinticRBFSurface(SimpleRBFSurface):
+    RBFSystem = QuinticRBFSystem
+
+
 class TPSSurface(SimpleRBFSurface):
     RBFSystem = ThinPlateRBFSystem
 
+
+"""
+class MultiQuadSurface(SimpleRBFSurface):
+    RBFSystem = MultiQuadraticRBFSystem
+
+
+class InvMultiQuadSurface(SimpleRBFSurface):
+    RBFSystem = InverseMultiQuadraticRBFSystem
+
+
+class GaussianSurface(SimpleRBFSurface):
+    RBFSystem = GaussianRBFSystem
+"""
 
 def toyf(x):
     if len(x.shape) == 1:

pySOT/search_procedure.py
@@ -418,7 +418,6 @@ class CandidateDYCORS(CandidateSRBF):
         for i in range(nlen):
             lower, upper = self.data.xlow[subset[i]], self.data.xup[subset[i]]
             ssigma = scalefactors[subset[i]]
-
             ind = np.where(ar[:, i] == 1)[0]
             xcand[ind, subset[i]] = stats.truncnorm.rvs(
                 (lower - xbest[subset[i]]) / ssigma, (upper - xbest[subset[i]]) / ssigma,
@@ -622,12 +621,14 @@ class GeneticAlgorithm(object):
         idx = np.sum(np.abs(self.proposed_points - x), axis=1).argmin()
         self.proposed_points = np.delete(self.proposed_points, idx, axis=0)
 
-    def next(self):
+    def next(self, objfunction=None):
+
         # Find a new point
-        def objfunction(x):
-            dist = np.atleast_2d(np.min(
-                scp.distance.cdist(x, self.proposed_points, metric='sqeuclidean',), axis=1)).T
-            return self.evals(x, scaling=False) + 10E6 * (dist < self.dtol ** 2)
+        if objfunction == None:
+            def objfunction(x):
+                dist = np.atleast_2d(np.min(
+                    scp.distance.cdist(x, self.proposed_points, metric='sqeuclidean',), axis=1)).T
+                return self.evals(x, scaling=False) + 10E6 * (dist < self.dtol ** 2)
 
         ga = GA(objfunction, self.data.dim, self.data.xlow,
                 self.data.xup, popsize=max([2*self.data.dim, 100]), ngen=100)
@@ -636,7 +637,7 @@ class GeneticAlgorithm(object):
         self.proposed_points = np.vstack((self.proposed_points, np.asarray(xnew)))
         return xnew
 
-    def make_points(self, xbest, sigma, evals, derivs):
+    def make_points(self, xbest, sigma, evals, derivs=None):
         self.evals = evals
         self.derivs = derivs
 

pySOT/sot_sync_strategies.py
@@ -26,8 +26,6 @@ from rbf_interpolant import RBFInterpolant
 # Get module-level logger
 logger = logging.getLogger(__name__)
 
-
-
 class SyncStrategyNoConstraints(BaseStrategy):
     """Parallel synchronous optimization strategy without non-bound constraints.
 
@@ -109,6 +107,9 @@ class SyncStrategyNoConstraints(BaseStrategy):
         # Start with first experimental design
         self.sample_initial()
 
+    def dist_to_unit(self, x):
+        return x / (np.sqrt(self.data.dim) * (self.data.xup - self.data.xlow))
+
     def log_completion(self, record):
         """Record a completed evaluation to the log.
 
@@ -170,7 +171,7 @@ class SyncStrategyNoConstraints(BaseStrategy):
             start_sample = np.vstack((start_sample, self.extra))
 
         start_sample = round_vars(self.data, start_sample)
-        for j in range(min(start_sample.shape[0], self.maxeval-self.numeval)):
+        for j in range(min(start_sample.shape[0], self.maxeval - self.numeval)):
             proposal = self.propose_eval(start_sample[j, :])
             self.resubmitter.rput(proposal)
 
@@ -187,16 +188,16 @@ class SyncStrategyNoConstraints(BaseStrategy):
         :param xx: Data points
         :return: Predicted function values
         """
-        return self.fhat.evals(xx)
+        return self.fhat.evals(self.dist_to_unit(xx))
 
     def derivs(self, xx):
-        return np.atleast_2d(self.fhat.deriv(xx))
+        return np.atleast_2d(self.fhat.deriv(self.dist_to_unit(xx)))
 
     def sample_adapt(self):
         """Generate and queue samples from the search strategy
         """
         self.adjust_step()
-        nsamples = min(self.nsamples, self.maxeval-self.numeval)
+        nsamples = min(self.nsamples, self.maxeval - self.numeval)
         self.search.make_points(self.xbest, self.sigma, self.evals, self.derivs)
         for _ in range(nsamples):
             proposal = self.propose_eval(np.ravel(self.search.next()))
@@ -234,7 +235,7 @@ class SyncStrategyNoConstraints(BaseStrategy):
         self.numeval += 1
         record.worker_id = self.worker_id
         record.worker_numeval = self.numeval
-        self.fhat.add_point(record.params[0], record.value)
+        self.fhat.add_point(self.dist_to_unit(record.params[0]), record.value)
         if record.value < self.fbest:
             self.xbest = record.params[0]
             self.fbest = record.value
@@ -318,7 +319,7 @@ class SyncStrategyPenalty(SyncStrategyNoConstraints):
         :return: Predicted function values
         """
         penalty = self.penalty_fun(xx)
-        vals = self.fhat.evals(xx)
+        vals = self.fhat.evals(self.dist_to_unit(xx))
         if scaling:
             ind = (np.where(penalty <= 0.0)[0]).T
             if ind.shape[0] > 1:
@@ -334,7 +335,7 @@ class SyncStrategyPenalty(SyncStrategyNoConstraints):
         constraints = np.array(self.data.eval_ineq_constraints(x))
         dconstraints = self.data.deriv_ineq_constraints(x)
         constraints[np.where(constraints < 0.0)] = 0.0
-        return np.atleast_2d(self.fhat.deriv(xx)) + \
+        return np.atleast_2d(self.fhat.deriv(self.dist_to_unit(xx))) + \
             2 * self.penalty * np.sum(
                 constraints * np.rollaxis(dconstraints, 2), axis=2).T
 
@@ -379,7 +380,7 @@ class SyncStrategyPenalty(SyncStrategyNoConstraints):
         self.numeval += 1
         record.worker_id = self.worker_id
         record.worker_numeval = self.numeval
-        self.fhat.add_point(record.params[0], record.value)
+        self.fhat.add_point(self.dist_to_unit(record.params[0]), record.value)
         # Check if the penalty function is a new best
         if record.value + penalty < self.fbest:
             self.xbest = record.params[0]

pySOT/test_problems.py
@@ -252,7 +252,7 @@ class Michalewicz:
         validate(self)
 
     def objfunction(self, x):
-        """Evaluate the Griewank function  at x
+        """Evaluate the Michalewicz function  at x
 
         :param x: Data point
         :return: Value at x
@@ -381,7 +381,7 @@ class Rosenbrock:
             raise ValueError('Dimension mismatch')
         total = 0
         for i in range(len(x) - 1):
-            total += 100 * (x[i]**2 - x[i+1])**2 + (x[i] - 1)**2
+            total += 100 * (x[i] ** 2 - x[i+1]) ** 2 + (x[i] - 1) ** 2
         return total
 
 
@@ -566,7 +566,7 @@ class StyblinskiTang:
         """
         if len(x) != self.dim:
             raise ValueError('Dimension mismatch')
-        return 0.5*sum(x**4 - 16*x**2 + 5*x)
+        return 0.5*np.sum(x ** 4 - 16 * x ** 2 + 5 * x)
 
 
 class Quartic: