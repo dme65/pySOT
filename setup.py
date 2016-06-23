480bc92b0a16377aed9397711ccd28759a2b80cd
David Eriksson <dme65@cornell.edu>
2016-06-23 11:57:29 -0400

    Updated documentation and made a new version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='pySOT',
-    version='0.1.20',
+    version='0.1.21',
     packages=['pySOT', 'pySOT.test'],
     url='http://pypi.python.org/pypi/pySOT/',
     license='LICENSE.txt',
     author='David Bindel, David Eriksson, Christine Shoemaker',
     author_email='bindel@cornell.edu, dme65@cornell.edu, shoemaker@nus.edu.sg',
     description='Surrogate Optimization Toolbox',
-    long_description=open('README.md').read(),	
+    long_description=open('README.md').read(),
     install_requires=['pyDOE', 'inspyred', 'pyKriging', 'POAP',
                       'py_dempster_shafer', 'subprocess32', 'matlab_wrapper'],
     classifiers=['Programming Language :: Python :: 2.7'],
