6a2dd0080829d34a615ac829135cc99445aa2012
David Eriksson <dme65@cornell.edu>
2016-06-27 15:37:20 -0400

    Added an MPI example with subprocesses

---
+++ pySOT/test/test_subprocess_mpi.py
@@ -0,0 +1,101 @@
+"""
+.. module:: test_subprocess_mpi
+  :synopsis: Test an external objective function
+.. moduleauthor:: David Eriksson <dme65@cornell.edu>
+"""
+
+try:
+    from mpi4py import MPI
+except:
+    print("You need to install mpi4py... Aborting")
+    exit()
+
+import logging
+from pySOT import *
+from poap.mpiserve import MPIMasterHub, MPIProcessWorker
+import numpy as np
+from subprocess32 import Popen, PIPE
+import os.path
+
+
+def array2str(x):
+    return ",".join(np.char.mod('%f', x))
+
+
+class CppSim(MPIProcessWorker):
+    def eval(self, record_id, params, extra_args=None):
+        self.process = Popen(['./sphere_ext', array2str(params[0])], stdout=PIPE)
+        val = np.nan
+        while True:
+            output = self.process.stdout.readline()
+            if output == '' and self.process.poll() is not None:  # No new output
+                break
+            if output:  # New intermediate output
+                try:
+                    val = float(output.strip())  # Try to parse output
+                except ValueError:  # If the output is nonsense we ignore it
+                    pass
+
+        rc = self.process.poll()  # Check the return code
+        if rc < 0 or np.isnan(val):
+            logging.info("WARNING: Incorrect output or crashed evaluation")
+            self.hub.finish_cancel(record_id)
+        else:
+            self.hub.finish_success(record_id, val)
+
+
+def main_worker():
+    CppSim().run()
+    logging.basicConfig(filename="./logfiles/test_subprocess_mpi.log",
+                        level=logging.INFO)
+
+
+def main_master(nworkers):
+    if not os.path.exists("./logfiles"):
+        os.makedirs("logfiles")
+    if os.path.exists("./logfiles/test_subprocess_mpi.log"):
+        os.remove("./logfiles/test_subprocess_mpi.log")
+    logging.basicConfig(filename="./logfiles/test_subprocess_mpi.log",
+                        level=logging.WARNING)
+
+    print("\nTesting the POAP MPI controller with {0} workers".format(nworkers))
+    print("Maximum number of evaluations: 200")
+    print("Search strategy: Candidate DYCORS")
+    print("Experimental design: Symmetric Latin Hypercube")
+    print("Surrogate: Cubic RBF")
+
+    assert os.path.isfile("./sphere_ext"), "You need to build sphere_ext"
+
+    maxeval = 200
+
+    data = Sphere(dim=10)
+    print(data.info)
+
+    # Create a strategy and a controller
+    strategy = \
+        SyncStrategyNoConstraints(
+            worker_id=0, data=data,
+            maxeval=maxeval, nsamples=nworkers,
+            exp_design=SymmetricLatinHypercube(dim=data.dim, npts=2*(data.dim+1)),
+            sampling_method=CandidateDYCORS(data=data, numcand=100*data.dim),
+            response_surface=RBFInterpolant(surftype=CubicRBFSurface, maxp=maxeval))
+
+    controller = MPIMasterHub(strategy)
+
+    # Run the optimization strategy
+    result = controller.optimize()
+    print('Best value found: {0}'.format(result.value))
+    print('Best solution found: {0}\n'.format(
+        np.array_str(result.params[0], max_line_width=np.inf,
+                     precision=5, suppress_small=True)))
+
+if __name__ == '__main__':
+    # Extract the rank
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    nprocs = comm.Get_size()
+
+    if rank == 0:
+        main_master(nprocs)
+    else:
+        main_worker()
