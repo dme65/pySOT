dc658534af3ec03875bd3a6492cd91832bb88bcb
David Eriksson <dme65@cornell.edu>
2016-08-03 23:35:43 -0500

    Changed to setup.py to use rst format for pypi
    pypi doesn’t understand markdown so I changed it so that the README is
    converted to rst for pypi before the upload takes place.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r","")
+except:
+    long_description = open('README.md').read()
 
 setup(
     name='pySOT',
-    version='0.1.23',
+    version='0.1.24',
     packages=['pySOT', 'pySOT.test'],
     url='http://pypi.python.org/pypi/pySOT/',
     license='LICENSE.txt',
     author='David Bindel, David Eriksson, Christine Shoemaker',
     author_email='bindel@cornell.edu, dme65@cornell.edu, shoemaker@nus.edu.sg',
     description='Surrogate Optimization Toolbox',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     install_requires=['pyDOE', 'inspyred', 'pyKriging', 'POAP==0.1.24',
                       'py_dempster_shafer', 'subprocess32', 'matlab_wrapper'],
     classifiers=['Programming Language :: Python :: 2.7'],
