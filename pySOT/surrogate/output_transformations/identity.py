8be34ee63b55e2cb07dd0a511fd3463d4ead27ab
David Eriksson <eriksson@uber.com>
2020-06-17 19:37:47 -0700

    Fix docs

--- pySOT/surrogate/output_transformations/identity.py
+++ pySOT/surrogate/output_transformations/identity.py
@@ -1,2 +1,10 @@
 def identity(x):
+    """Identity mapping.
+
+    :param x: Array to be transformed
+    :type x: numpy.array
+
+    :return: x
+    :rtype: numpy.array
+    """
     return x
--- pySOT/surrogate/output_transformations/median_capping.py
+++ pySOT/surrogate/output_transformations/median_capping.py
@@ -2,6 +2,14 @@ import numpy as np
 
 
 def median_capping(x):
+    """Replace values above the median by the median.
+
+    :param x: Array to be transformed
+    :type x: numpy.array
+
+    :return: x
+    :rtype: numpy.array
+    """
     x = x.copy()
     medf = np.median(x)
     x[x > medf] = medf
